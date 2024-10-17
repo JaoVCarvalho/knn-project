0f80e594ff39eb4541a2461f88139d13a209a5cf
diogocarrer <diogocarrer14@gmail.com>
2024-10-17 16:22:20 -0300

    ajustando para todos os k

--- KnnLib.py
+++ KnnLib.py
@@ -8,12 +8,13 @@ X,y = load_iris(return_X_y=True)
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=13)
 
-n_neighbors = 7
+for k in [1, 3, 5, 7]:
+    print(f"Resultados para k = {k}:")
 
-clf = KNeighborsClassifier(n_neighbors=n_neighbors)
+    clf = KNeighborsClassifier(n_neighbors=k)
 
-clf.fit(X_train,y_train)
+    clf.fit(X_train,y_train)
 
-y_predict = clf.predict(X_test)
+    y_predict = clf.predict(X_test)
 
-print(classification_report(y_test, y_predict, target_names=iris.target_names))
+    print(classification_report(y_test, y_predict, target_names=iris.target_names))
